dc094220be66f072523ac18c22cc38ce44226f25
NabinIslam <sinabin62246@gmail.com>
2024-10-14 13:30:56 +0600

    fix textarea error

--- src/components/pages/about_us/GetInTouchContactForm.tsx
+++ src/components/pages/about_us/GetInTouchContactForm.tsx
@@ -5,27 +5,28 @@ const GetInTouchContactForm = () => {
   return (
     <form className="space-y-5" action="">
       <Input
-        className="focus:ring-primary rounded-sm"
+        className="rounded-sm focus:ring-primary"
         type="text"
         placeholder="Full name"
         required
       />
       <Input
-        className="focus:ring-primary rounded-sm"
+        className="rounded-sm focus:ring-primary"
         type="email"
         placeholder="Email"
         required
       />
       <Input
-        className="focus:ring-primary rounded-sm"
+        className="rounded-sm focus:ring-primary"
         type="tel"
         placeholder="Phone number"
         required
       />
       <Textarea
-        className="focus:ring-primary rounded-sm"
+        className="rounded-sm focus:ring-primary"
         placeholder="Write your massage"
         rows={5}
+        required
       />
       <button
         className="flex items-center justify-center gap-x-2 rounded-sm bg-primary px-5 py-3 text-sm font-medium text-white duration-300 hover:bg-secondary hover:text-white"
