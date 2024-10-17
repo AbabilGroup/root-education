c766916427b159bf994e8ee74d43963c34dc916f
NabinIslam <sinabin62246@gmail.com>
2024-10-17 19:26:28 +0600

    Pre-departure Briefing page

---
+++ src/components/pages/services/pre_departure_briefing/MainSection.tsx
@@ -0,0 +1,46 @@
+import FadeInLeftWithSlowBounce from "@/animation_wrappers/FadeInLeftWithSlowBounce";
+import FadeInRightWithSlowBounce from "@/animation_wrappers/FadeInRightWithSlowBounce ";
+import SiteButton from "@/components/ui/SiteButton";
+import Image from "next/image";
+
+const MainSection = () => {
+  return (
+    <section className="pb-[50px] pt-[100px]">
+      <div className="container flex flex-col items-center justify-between gap-x-20 gap-y-10 lg:flex-row">
+        <FadeInLeftWithSlowBounce className="basis-full space-y-5 xl:basis-1/2">
+          <div className="flex items-center justify-center lg:justify-start">
+            <span className="inline-block rounded-full border border-primary px-5 py-3 text-xl font-medium text-primary">
+              Pre-departure Travel Guide
+            </span>
+          </div>
+          <h2 className="text-center text-4xl font-semibold lg:text-left">
+            Complete Pre-departure Guidance for Study Abroad
+          </h2>
+          <p className="text-center text-sm font-medium lg:text-left">
+            Our Complete Pre-departure Guidance for Study Abroad service is
+            designed to help you feel confident and well-prepared before leaving
+            for your international studies. We cover everything from important
+            travel documents, visa and health requirements to cultural insights,
+            packing tips, and financial advice. Our comprehensive support
+            ensures a smooth transition, so you can focus on making the most of
+            your study abroad experience from day one. Let us guide you every
+            step of the way!
+          </p>
+          <div className="flex items-center justify-center lg:justify-start">
+            <SiteButton>Book Your Consultation</SiteButton>
+          </div>
+        </FadeInLeftWithSlowBounce>
+        <FadeInRightWithSlowBounce className="relative min-h-[400px] w-full basis-full xl:min-h-[460px] xl:basis-1/2">
+          <Image
+            className="cursor-pointer object-contain object-center xl:object-right"
+            src="/images/pages/services/pre_departure_briefing/pre_departure_briefing.svg"
+            alt="University Selection"
+            fill
+          />
+        </FadeInRightWithSlowBounce>
+      </div>
+    </section>
+  );
+};
+
+export default MainSection;
