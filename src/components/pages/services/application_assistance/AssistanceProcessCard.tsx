351d4a6375b1dd100d44b598e067994d5d6278ac
NabinIslam <sinabin62246@gmail.com>
2024-10-21 16:19:17 +0600

    assistance card

--- src/components/pages/services/application_assistance/AssistanceProcessCard.tsx
+++ src/components/pages/services/application_assistance/AssistanceProcessCard.tsx
@@ -1,34 +1,37 @@
+import FadeInRightWithSlowBounce from "@/animation_wrappers/FadeInRightWithSlowBounce ";
 import Image from "next/image";
 
-const AssistanceProcessCard = () => {
-  return (
-    <div className="relative flex max-w-[430px] items-center justify-start">
-      {/* left  */}
+type TAssistanceProcessCardProps = {
+  stepNo: number;
+  iconNo: number;
+  title: string;
+  description: string;
+};
 
-      <div className="space-y-3 rounded-lg border border-b-8 border-b-primary px-8 py-4 shadow">
-        <div className="-mt-[3.5rem] flex items-center justify-center">
+const AssistanceProcessCard = ({
+  stepNo,
+  iconNo,
+  title,
+  description,
+}: TAssistanceProcessCardProps) => {
+  return (
+    <FadeInRightWithSlowBounce>
+      <div className="mx-auto h-auto w-44 basis-auto rounded-t-lg bg-primary py-2 text-center text-sm font-medium text-white">
+        Step 0{stepNo}
+      </div>
+      <div className="min-h-[220px] space-y-3 rounded-lg border border-b-8 border-b-primary px-8 py-4 shadow">
+        <div className="flex items-center justify-center">
           <Image
-            src="/images/pages/services/application_assistance/process/1.svg"
+            src={`/images/pages/services/application_assistance/process/${iconNo}.svg`}
             alt="Initial Consultation"
-            height={80}
-            width={80}
+            height={60}
+            width={60}
           />
         </div>
-        <h5 className="text-center text-2xl font-medium">
-          Initial Consultation
-        </h5>
-        <p className="text-center text-sm font-medium">
-          Schedule a one-on-one session to assess the student’s needs, goals,
-          and preferred study destinations.
-        </p>
+        <h5 className="text-center text-2xl font-medium">{title}</h5>
+        <p className="text-center text-sm font-medium">{description}</p>
       </div>
-
-      {/* right */}
-
-      <div className="m-0 h-auto w-32 -rotate-90 rounded-b-lg border bg-primary py-2 text-center text-sm font-medium text-white">
-        Step 01
-      </div>
-    </div>
+    </FadeInRightWithSlowBounce>
   );
 };
 
--- src/components/pages/services/pre_departure_briefing/DepartureCard.tsx
+++ src/components/pages/services/pre_departure_briefing/DepartureCard.tsx
@@ -1,32 +1,32 @@
 import FadeInRightWithSlowBounce from "@/animation_wrappers/FadeInRightWithSlowBounce ";
 import Image from "next/image";
 
-type TAssistanceProcessCardProps = {
+type TDepartureCardProps = {
   stepNo: number;
   iconNo: number;
   title: string;
   description: string;
 };
 
-const AssistanceProcessCard = ({
+const DepartureCard = ({
   stepNo,
   iconNo,
   title,
   description,
-}: TAssistanceProcessCardProps) => {
+}: TDepartureCardProps) => {
   return (
     <FadeInRightWithSlowBounce>
       <div className="mx-auto h-auto w-44 basis-auto rounded-t-lg bg-primary py-2 text-center text-sm font-medium text-white">
         Step 0{stepNo}
       </div>
 
-      <div className="basis-full space-y-3 rounded-lg border border-b-8 border-b-primary px-8 py-4 shadow min-h-[240px]">
+      <div className="min-h-[240px] basis-full space-y-3 rounded-lg border border-b-8 border-b-primary px-8 py-4 shadow">
         <div className="flex items-center justify-center">
           <Image
             src={`/images/pages/services/pre_departure_briefing/process/${iconNo}.svg`}
             alt="Initial Consultation"
-            height={80}
-            width={80}
+            height={60}
+            width={60}
           />
         </div>
         <h5 className="text-center text-2xl font-medium">{title}</h5>
@@ -36,4 +36,4 @@ const AssistanceProcessCard = ({
   );
 };
 
-export default AssistanceProcessCard;
+export default DepartureCard;
