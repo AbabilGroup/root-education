db240b7ba88cdfc8d043a56ae78c6933d32e3693
NabinIslam <sinabin62246@gmail.com>
2024-10-21 20:22:44 +0600

    university details page initialize

---
+++ src/app/(CommonLayout)/universities/[university]/page.tsx
@@ -0,0 +1,12 @@
+import PageBanner from "@/components/common/PageBanner";
+
+const UniversityDetailsPage = () => {
+  return (
+    <main>
+      <PageBanner PageName="Spiru haret University, Romania" />
+      <h1>This is UniversityDetailsPage component</h1>
+    </main>
+  );
+};
+
+export default UniversityDetailsPage;
