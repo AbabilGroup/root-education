218889bc5f997289d935e51d99124856618664d3
NabinIslam <sinabin62246@gmail.com>
2024-10-28 15:42:20 +0600

    fix error: Type error: Binding element 'countryInfo' implicitly has an 'any' type.

--- src/components/pages/countries/country_details/AdmissionRequirements.tsx
+++ src/components/pages/countries/country_details/AdmissionRequirements.tsx
@@ -1,6 +1,7 @@
+import { Root } from "@/types";
 import AdmissionRequirementsCard from "./AdmissionRequirementsCard";
 
-const AdmissionRequirements = ({ countryInfo }) => {
+const AdmissionRequirements = ({ countryInfo }: {countryInfo: Root}) => {
   return (
     <div className="mt-10 grid grid-cols-1 gap-10 lg:grid-cols-2 xl:grid-cols-3">
       {countryInfo.admission_requirments[0].list.map((requirment) => (
