60238553f2a9fbec9512e4f067966869f0e16670
NabinIslam <sinabin62246@gmail.com>
2024-10-28 15:55:19 +0600

    fix error: Type error: Binding element 'countryInfo' implicitly has an 'any' type.

--- src/components/pages/countries/country_details/ApplicationProcedures.tsx
+++ src/components/pages/countries/country_details/ApplicationProcedures.tsx
@@ -1,6 +1,7 @@
+import { Root } from "@/types";
 import ApplicationProceduresCard from "./ApplicationProceduresCard";
 
-const ApplicationProcedures = ({ countryInfo }) => {
+const ApplicationProcedures = ({ countryInfo }: { countryInfo: Root }) => {
   return (
     <div className="mt-10 grid grid-cols-1 gap-10 xl:grid-cols-2">
       {countryInfo.application_procedures[0].applicationprocedureslist.map(
