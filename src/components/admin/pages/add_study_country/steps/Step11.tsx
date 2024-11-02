41ce09c65f37695ca7a927319292555ba87a9fcc
NabinIslam <sinabin62246@gmail.com>
2024-11-02 12:33:23 +0600

    change api url

--- src/components/admin/pages/add_study_country/steps/Step11.tsx
+++ src/components/admin/pages/add_study_country/steps/Step11.tsx
@@ -1,76 +1,84 @@
+"use client";
+
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { TabsContent } from "@/components/ui/tabs";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
-import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+import { useForm, useFieldArray, SubmitHandler } from "react-hook-form";
+import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
+
+type Faq = {
+  question: string;
+  answer: string;
+};
+
+type FormValues = {
+  faqs: Faq[];
+};
 
 const Step11 = () => {
-  const [faqFields, setFaqFields] = useState([
-    { id: 1, questionPlaceholder: "Question 1", answerPlaceholder: "Answer 1" },
-  ]);
+  const { control, register, handleSubmit } = useForm<FormValues>({
+    defaultValues: {
+      faqs: [{ question: "", answer: "" }],
+    },
+  });
 
-  const addFaqField = () => {
-    setFaqFields((prev) => [
-      ...prev,
-      {
-        id: prev.length + 1,
-        questionPlaceholder: `Question ${prev.length + 1}`,
-        answerPlaceholder: `Answer ${prev.length + 1}`,
-      },
-    ]);
-  };
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "faqs",
+  });
 
-  const removeFaqField = (id: number) => {
-    setFaqFields((prev) => prev.filter((field) => field.id !== id));
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
   };
 
   return (
     <TabsContent value="step11">
-      <form className="w-1/2 space-y-5" action="">
-        {/* Why study  */}
-        <div>
-          <Label>FAQ</Label>
-
-          <Textarea className="mb-2" placeholder="Short brief" required />
-
-          {faqFields.map((field, index) => (
-            <div key={field.id} className="mb-2 space-y-2">
-              <div className="flex items-center space-x-2">
+      <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
+        <Label>FAQ</Label>
+        {fields.map((field, index) => (
+          <div
+            className="flex items-center justify-between gap-x-3"
+            key={field.id}
+          >
+            <div className="basis-full space-y-3">
+              <div>
                 <Input
                   type="text"
-                  placeholder={field.questionPlaceholder}
+                  {...register(`faqs.${index}.question` as const)}
+                  placeholder="Question"
                   required
                 />
-                {field.id === faqFields.length && (
-                  <button
-                    type="button"
-                    onClick={addFaqField}
-                    className="rounded-full bg-blue-500 p-2 text-white"
-                  >
-                    <CiCirclePlus />
-                  </button>
-                )}
-                {index > 0 && (
-                  <button
-                    type="button"
-                    onClick={() => removeFaqField(field.id)}
-                    className="rounded-full bg-red-500 p-2 text-white"
-                  >
-                    <CiCircleMinus />
-                  </button>
-                )}
               </div>
-              <Textarea
-                rows={5}
-                placeholder={field.answerPlaceholder}
-                required
-              />
+              <div>
+                <Textarea
+                  {...register(`faqs.${index}.answer` as const)}
+                  rows={5}
+                  placeholder="Answer"
+                  required
+                />
+              </div>
             </div>
-          ))}
+            <button
+              className="basis-auto rounded-full bg-primary p-1"
+              type="button"
+              onClick={() => remove(index)}
+            >
+              <FaMinusCircle className="text-xl text-white" />
+            </button>
+          </div>
+        ))}
+        <button
+          className="rounded-full bg-primary p-1"
+          type="button"
+          onClick={() => append({ question: "", answer: "" })}
+        >
+          <FaPlusCircle className="text-xl text-white" />
+        </button>
+        <div>
+          <Button type="submit">Next</Button>
         </div>
-        <Button type="submit">Post</Button>
       </form>
     </TabsContent>
   );
