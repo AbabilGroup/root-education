fb27e252b13484277d662a848f25c8a31ffff0d1
NabinIslam <sinabin62246@gmail.com>
2024-11-12 19:11:20 +0600

    fix AdmissionRequirment error

--- src/components/pages/countries/country_details/AdmissionRequirements.tsx
+++ src/components/pages/countries/country_details/AdmissionRequirements.tsx
@@ -4,7 +4,7 @@ import AdmissionRequirementsCard from "./AdmissionRequirementsCard";
 const AdmissionRequirements = ({ countryInfo }: { countryInfo: Root }) => {
   return (
     <div className="mt-10 grid grid-cols-1 gap-10 lg:grid-cols-2 xl:grid-cols-3">
-      {countryInfo.admission_requirments[0].list.map((requirement) => (
+      {countryInfo.admission_requirments.list.map((requirement) => (
         <AdmissionRequirementsCard
           key={requirement.title}
           requirement={requirement}
