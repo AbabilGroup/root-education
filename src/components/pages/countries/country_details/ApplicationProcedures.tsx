e6888818d5bb22187413fdaca37190ce95a751fc
NabinIslam <sinabin62246@gmail.com>
2024-11-12 19:13:17 +0600

    fix ApplicationProcedures error

--- src/components/pages/countries/country_details/ApplicationProcedures.tsx
+++ src/components/pages/countries/country_details/ApplicationProcedures.tsx
@@ -4,7 +4,7 @@ import ApplicationProceduresCard from "./ApplicationProceduresCard";
 const ApplicationProcedures = ({ countryInfo }: { countryInfo: Root }) => {
   return (
     <div className="mt-10 grid grid-cols-1 gap-10 xl:grid-cols-2">
-      {countryInfo.application_procedures[0].applicationprocedureslist.map(
+      {countryInfo.application_procedures.applicationprocedureslist.map(
         (procedure, index) => (
           <ApplicationProceduresCard
             key={procedure.title}
