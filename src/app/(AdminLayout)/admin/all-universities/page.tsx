bbead3fd0f3c3de0958fff51fa18e02ea0e253df
NabinIslam <sinabin62246@gmail.com>
2024-11-15 18:22:53 +0600

    add prop

--- src/app/(AdminLayout)/admin/all-universities/page.tsx
+++ src/app/(AdminLayout)/admin/all-universities/page.tsx
@@ -1,9 +1,12 @@
 import AllUniversityTable from "@/components/admin/pages/all_universities/AllUniversityTable";
+import { getAllUniversities } from "@/services/getAllUniversities";
 
-const AllUniversitiesPage = () => {
+const AllUniversitiesPage = async () => {
+  const universities = await getAllUniversities();
+
   return (
     <main className="p-10">
-      <AllUniversityTable />
+      <AllUniversityTable universities={universities} />
     </main>
   );
 };
--- src/services/getAllUniversities.ts
+++ src/services/getAllUniversities.ts
@@ -1,12 +1,12 @@
 import { apiUrl } from "@/secrets";
 
-export const getAllCountries = async () => {
-  const res = await fetch(`${apiUrl}/study_country`, {
+export const getAllUniversities = async () => {
+  const res = await fetch(`${apiUrl}/all_universities`, {
     cache: "no-store",
   });
 
   if (!res.ok) {
-    throw new Error("Failed to fetch countries ");
+    throw new Error("Failed to fetch universities");
   }
 
   return res.json();
