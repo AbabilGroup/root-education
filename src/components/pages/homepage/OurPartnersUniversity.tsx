5bec03a573333eb54629d02dd8558a8276be56bd
NabinIslam <sinabin62246@gmail.com>
2024-11-23 12:26:30 +0600

    fix type error

--- src/components/pages/homepage/OurPartnersUniversity.tsx
+++ src/components/pages/homepage/OurPartnersUniversity.tsx
@@ -2,7 +2,6 @@ import Section from "@/components/common/Section";
 import UniversityCard from "@/components/common/UniversityCard";
 import { Button } from "@/components/ui/Button";
 import { getAllUniversities } from "@/services/getAllUniversities";
-import { University } from "@/types/university";
 import Link from "next/link";
 
 const OurPartnersUniversity = async () => {
@@ -14,9 +13,24 @@ const OurPartnersUniversity = async () => {
       title="World-Class Educational Collaborations"
     >
       <div className="container grid grid-cols-1 gap-x-10 gap-y-10 lg:grid-cols-2 xl:grid-cols-3">
-        {universities.map((university: University) => (
-          <UniversityCard key={university.id} university={university} />
-        ))}
+        {universities.map(
+          (university: {
+            id: number;
+            name: string;
+            description: string;
+            slug: string;
+            photo: string;
+            logo: string;
+            short_info: {
+              country: string;
+              university_type: string;
+              total_students: number;
+              launched: number;
+            };
+          }) => (
+            <UniversityCard key={university.id} university={university} />
+          ),
+        )}
       </div>
       <div className="mt-10 flex items-center justify-center">
         <Link href="/universities">
