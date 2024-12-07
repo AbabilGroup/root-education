6293c2f13d71f691be5222ed39a644e03800ae4f
NabinIslam <sinabin62246@gmail.com>
2024-12-07 15:09:21 +0600

    no store

--- src/services/getAllUniversities.ts
+++ src/services/getAllUniversities.ts
@@ -7,7 +7,9 @@ export const getAllUniversities = async (searchParams = {}) => {
   // Append the query string to the API URL
   const url = `${apiUrl}/all_university/${queryString ? `?${queryString}` : ""}`;
 
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch universities");
